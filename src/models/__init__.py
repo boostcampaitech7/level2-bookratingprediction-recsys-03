91f49444ea1d68a223f5bd26658adfe8f72bb1c4
0-virus <pyk09667@gmail.com>
2024-11-05 04:20:21 +0000

    feat: upload baseline files with update

---
+++ src/models/__init__.py
@@ -0,0 +1,11 @@
+from .FM import FactorizationMachine as FM
+from .FFM import FieldAwareFactorizationMachine as FFM
+from .DeepFM import DeepFM
+from .NCF import NeuralCollaborativeFiltering as NCF
+from .WDN import WideAndDeep as WDN
+from .DCN import DeepCrossNetwork as DCN
+from .FM_Image import Image_FM, Image_DeepFM, ResNet_DeepFM
+from .FM_Text import Text_FM, Text_DeepFM
+from catboost import CatBoostRegressor as CatBoost
+from xgboost import XGBRegressor as XGBoost
+from lightgbm import LGBMRegressor as LightGBM
